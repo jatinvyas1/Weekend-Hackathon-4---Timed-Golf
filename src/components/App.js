496d1d3f18ecd4029d3766d394cfb1329231d945
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-17 17:25:30 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,49 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.state = {
+      time: 0,
+      x: 0,
+      y: 0,
+      start: false,
+      ballPosition: { top: "0px", left: "0px" }
+    };
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    // this.shiftBall = this.shiftBall.bind(this);
   }
+
+  buttonClickHandler() {
+    this.interval = setInterval(() => {
+      this.setState({ time: this.state.time + 1 });
+    }, 1000);
+    console.log(this.state.time);
+    this.setState({ start: true });
+  }
+
   componentDidMount() {
-    
+    document.addEventListener("keydown", this.shiftBall);
   }
 
-  componentWillUnmount() {
-    
-  }
-
-
+  componentWillUnmount() {}
 
   render() {
     return (
- <>
-</>
+      <>
+        <p className="heading-timer">{this.state.time}</p>
+        {this.state.start ? (
+          <>
+            <div className="ball" style={this.state.ballPosition}></div>
+            <div className="hole"></div>
+          </>
+        ) : (
+          <button
+            className="start ballProvider"
+            onClick={this.buttonClickHandler}
+          >
+            Start
+          </button>
+        )}
+      </>
     );
   }
 }
