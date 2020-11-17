ebaa58892168888db8c6e103c0100e59fa6ca840
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-17 17:45:45 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,32 @@ class Timer extends React.Component {
       time: 0,
       x: 0,
       y: 0,
-      start: false,
-      ballPosition: { top: "0px", left: "0px" }
+      start: false
     };
     this.buttonClickHandler = this.buttonClickHandler.bind(this);
     this.shiftBall = this.shiftBall.bind(this);
   }
-  shiftBall() {}
+  shiftBall(event) {
+    event.preventDefault();
+    if (!this.state.start) {
+      return;
+    }
+    if (event.keyCode === 37) {
+      this.setState({ y: this.state.y - 5 });
+    }
+    if (event.keyCode === 39) {
+      this.setState({ y: this.state.y + 5 });
+    }
+    if (event.keyCode === 38) {
+      this.setState({ x: this.state.x - 5 });
+    }
+    if (event.keyCode === 40) {
+      this.setState({ x: this.state.x + 5 });
+    }
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.interval);
+    }
+  }
   buttonClickHandler() {
     this.interval = setInterval(() => {
       this.setState({ time: this.state.time + 1 });
@@ -23,7 +42,7 @@ class Timer extends React.Component {
   }
 
   componentDidMount() {
-    document.addEventListener("keydown", this.shiftBall);
+    document.addEventListener("keydown", (event) => this.shiftBall(event));
   }
 
   componentWillUnmount() {}
@@ -33,7 +52,10 @@ class Timer extends React.Component {
       <>
         <p className="heading-timer">{this.state.time}</p>
 
-        <div className="ball" style={this.state.ballPosition}></div>
+        <div
+          className="ball"
+          style={{ top: this.state.x + "px", left: this.state.y + "px" }}
+        ></div>
         <div className="hole"></div>
         {!this.state.start && (
           <button
